62c850cc51d4fc52d5458f92581c238d3bb7b3b2
kukutt <pan3yao@gmail.com>
2020-04-11 14:04:10 +0800

    测试代码，api执行action测试 5

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+
+sudo apt-get update
+sudo apt-get -y install jq
+
+# api变量解析
+jsshell=`cat $GITHUB_EVENT_PATH | jq ".action" | sed 's/\"//g'`
+
 mkdir -p output
-echo "start run $github.event.event_type" >> output/log.txt
+if [ ! -f "$jsshell.sh" ];then
+    echo "start run $jsshell" >> output/log.txt
+else
+    echo "not find $jsshell !!" >> output/log.txt
+fi
+
 ./test.sh output
 #./openwrt.sh output
 #./socat.sh output
--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "i've done nothing"
-export >> $1/log.txt
-env >> $1/log.txt
-sudo cat $GITHUB_EVENT_PATH >> $1/log.txt
+
+# api变量解析
+jstxt=`cat $GITHUB_EVENT_PATH | jq ".client_payload.txt" | sed 's/\"//g'`
+
+
+# 正常执行
+echo [$jstxt] >> $1/log.txt
+#export >> $1/log.txt
+#env >> $1/log.txt
+#sudo cat $GITHUB_EVENT_PATH >> $1/log.txt
 #sudo cp  $GITHUB_EVENT_PATH >> $1/
