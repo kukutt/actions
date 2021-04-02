6dc363f5643aaabb33b1c678d1ba5c7787e571e3
kukutt <63354888+kukutt@users.noreply.github.com>
2021-04-02 16:40:01 +0800

    Update run.sh

--- macos/run.sh
+++ macos/run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -e
 
-ifconfig
-netstat -ant
-
-brew update
-brew install jq
+#brew update
+#brew install jq
 
 # api变量解析
 jssshport=`cat $GITHUB_EVENT_PATH | jq ".inputs.sshport" | sed 's/\"//g'`
@@ -14,4 +11,36 @@ jsfrpserver=`cat $GITHUB_EVENT_PATH | jq ".inputs.frpserver" | sed 's/\"//g'`
 jsfrpport=`cat $GITHUB_EVENT_PATH | jq ".inputs.frpport" | sed 's/\"//g'`
 jsfrptk=`cat $GITHUB_EVENT_PATH | jq ".inputs.frptk" | sed 's/\"//g'`
 
-echo $jsfrptk
+mkdir -p bin
+# 准备frp
+wget https://github.com/fatedier/frp/releases/download/v0.20.0/frp_0.20.0_darwin_amd64.tar.gz
+tar -zxvf frp_0.20.0_darwin_amd64.tar.gz
+cp ./frp_0.20.0_darwin_amd64.tar.gz/frpc ./bin/
+
+# 修改用户名
+sudo echo $jssshuser:$jssshpwd | sudo chpasswd
+
+rm -rf frpc.ini
+cat >> frpc.ini <<EOF
+[common]
+server_addr = $jsfrpserver
+server_port = $jsfrpport
+privilege_token = $jsfrptk
+[ss_actions_ssh]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 22
+remote_port = $jssshport
+EOF
+./bin/frpc -c frpc.ini > /dev/null 2>&1 &
+
+
+echo user[$jssshuser]
+sleep 300
+while [ -f "$HOME/run" ]
+do
+    sleep 60
+done
+sleep 60
+
+killall frpc
