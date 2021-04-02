6ae135a1f9de474845b9b696bf9c64039187247a
kukutt <63354888+kukutt@users.noreply.github.com>
2021-04-02 16:32:10 +0800

    Update run.sh

--- macos/run.sh
+++ macos/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -e
 
-cat $GITHUB_EVENT_PATH
 ifconfig
 netstat -ant
+
+brew update
+brew install jq
+
+# api变量解析
+jssshport=`cat $GITHUB_EVENT_PATH | jq ".inputs.sshport" | sed 's/\"//g'`
+jssshuser=`whoami`
+jssshpwd=`cat $GITHUB_EVENT_PATH | jq ".inputs.sshpwd" | sed 's/\"//g'`
+jsfrpserver=`cat $GITHUB_EVENT_PATH | jq ".inputs.frpserver" | sed 's/\"//g'`
+jsfrpport=`cat $GITHUB_EVENT_PATH | jq ".inputs.frpport" | sed 's/\"//g'`
+jsfrptk=`cat $GITHUB_EVENT_PATH | jq ".inputs.frptk" | sed 's/\"//g'`
+
+echo $jsfrptk
