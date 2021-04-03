19c4e2454ada8665419599522da94fd2934cc797
kukutt <63354888+kukutt@users.noreply.github.com>
2021-04-03 21:31:48 +0800

    Update run.sh

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -17,7 +17,7 @@ tar -zxvf frp_0.20.0_linux_amd64.tar.gz
 cp ./frp_0.20.0_linux_amd64/frpc ./bin/
 
 # 添加用户
-sudo adduser -s /bin/bash username
+sudo adduser -s /bin/bash panyao
 sudo usermod -aG sudo panyao
 sudo mkdir /home/panyao
 sudo chown panyao:panyao /home/panyao
